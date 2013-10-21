cafb088d7299e69e8e1234457a21e7ab7883fe90
MartianZ <fzyadmin@gmail.com>
2013-10-21 15:34:51 +0800

    define some contants in constants.h

---
+++ fakeThunder/fakeThunder/Constants.h
@@ -0,0 +1,25 @@
+//
+//  Constants.h
+//  fakeThunder
+//
+//  Created by Martian Z on 13-10-21.
+//  Copyright (c) 2013年 MartianZ. All rights reserved.
+//
+
+#ifndef fakeThunder_Constants_h
+#define fakeThunder_Constants_h
+
+#define UD_FIRST_STARTUP @"UserDefaultFirstTimeStartup"
+#define UD_SAVE_PATH @"UserDefaultSavePath"
+#define UD_SAVE_PATH_DISPLAY @"UserDefaultSavePathDisplay"
+
+#define UD_MAX_TASKS @"UserDefaultMaxTasks"
+#define UD_MAX_THREADS @"UserDefaultMaxThreads"
+
+#define UD_TASK_SPEED_LIMIT @"UserDefaultTaskLimit"
+
+#define UD_VOD_PLAY_SHARPNESS @"UserDefaultVodSharpness"
+
+#define UD_NOTIFICATION @"UserDefaultSendNortification"
+
+#endif

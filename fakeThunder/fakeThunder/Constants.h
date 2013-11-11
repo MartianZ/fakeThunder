374994987ed0ee18c06739a5f9fa578932132fa1
MartianZ <fzyadmin@gmail.com>
2013-11-11 19:50:21 +0800

    update gpl v3 license

--- fakeThunder/fakeThunder/Constants.h
+++ fakeThunder/fakeThunder/Constants.h
@@ -5,7 +5,22 @@
 //  Created by Martian Z on 13-10-21.
 //  Copyright (c) 2013年 MartianZ. All rights reserved.
 //
-
+/*
+ Copyright (C) 2012-2014 MartianZ
+ 
+ fakeThunder is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ fakeThunder is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef fakeThunder_Constants_h
 #define fakeThunder_Constants_h
 
